d50adcbd345e9118046fa20b9d5c35a278cf28c0
Alexander Semionov <warlockxins@gmail.com>
2018-07-21 23:38:58 +0200

    better data processing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 new Vue({
   el: "#app",
   data: {
-    data: undefined
+    data: undefined,
+    dataHeaders: undefined
   },
   methods: {
     setData: function(results) {
-      this.data = results;
-    }
+      this.dataHeaders = results.data[0].map(function(item, index) {
+        return {
+          name: item,
+          use: true,
+          select: "in", //out
+          index: index
+        };
+      });
+
+      this.data = results.data.slice(1, 5);
+    },
+    startLearning: function() {}
   }
 });
--- src/table-vue.js
+++ src/table-vue.js
@@ -1,12 +1,4 @@
 Vue.component("data-table", {
   template: "#table-template",
-  computed: {
-    headers: function() {
-      return this.dataset.data[0];
-    },
-    data: function() {
-      return this.dataset.data.slice(1, 5);
-    }
-  },
-  props: ["dataset"]
+  props: ["dataset", "headers"]
 });
