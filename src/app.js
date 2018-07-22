533c128f72d936d1cf2137f25f50de3a5e3a5dcd
Alexander Semionov <warlockxins@gmail.com>
2018-07-22 15:41:43 +0200

    optimize code and filter input/output nodes for Neural net

--- src/app.js
+++ src/app.js
@@ -4,6 +4,48 @@ new Vue({
     data: undefined,
     dataHeaders: undefined
   },
+  computed: {
+    usedHeaders: function() {
+      if (!this.dataHeaders) return [];
+
+      return this.dataHeaders.filter(function(item) {
+        return item.use === true;
+      });
+    },
+    inputHeaders: function() {
+      return this.getNodes("in");
+    },
+    outputHeaders: function() {
+      return this.getNodes("out");
+    },
+    previewData: function() {
+      return this.data && this.data.slice(1, 5);
+    },
+    hiddenNodeCount: function() {
+      return Math.ceil(
+        ((this.inputHeaders.length + this.outputHeaders.length) * 2) / 3
+      );
+    },
+    validation: function() {
+      if (this.inputHeaders.length === 0) {
+        return {
+          valid: false,
+          message: "input node count cannot be 0"
+        };
+      }
+
+      if (this.outputHeaders.length === 0) {
+        return {
+          valid: false,
+          message: "output node count cannot be 0"
+        };
+      }
+
+      return {
+        valid: true
+      };
+    }
+  },
   methods: {
     setData: function(results) {
       this.dataHeaders = results.data[0].map(function(item, index) {
@@ -15,10 +57,23 @@ new Vue({
         };
       });
 
-      this.data = results.data.slice(1, 5);
+      this.data = results.data;
+    },
+    getNodes: function(isInput) {
+      return this.usedHeaders.filter(function(item) {
+        return item.select == isInput;
+      });
     },
     startLearning: function() {
+      if (!this.validation.valid) {
+        return;
+      }
       var net = new neuralNet.NeuralNet();
+      net.setTopology([
+        this.inputHeaders.length,
+        this.hiddenNodeCount,
+        this.outputHeaders.length
+      ]);
       console.log("net", net);
     }
   }
