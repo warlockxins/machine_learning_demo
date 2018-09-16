7f81a79e456e4fe526611b198677abf1ca690b66
Alexander Semionov <warlockxins@gmail.com>
2018-09-16 22:11:38 +0200

    map non number values in ML results

--- src/app.js
+++ src/app.js
@@ -68,13 +68,54 @@ new Vue({
       if (!this.validation.valid) {
         return;
       }
-      var net = new neuralNet.NeuralNet();
-      net.setTopology([
-        this.inputHeaders.length,
-        this.hiddenNodeCount,
-        this.outputHeaders.length
-      ]);
+      console.log("input headers", this.inputHeaders);
+      console.log("output headers", this.outputHeaders);
+      var net = new ml.NeuralNet();
+      net.setTopology(
+        [
+          this.inputHeaders.length,
+          this.hiddenNodeCount,
+          this.outputHeaders.length
+        ],
+        ml.transferFunction.tangent
+      );
       console.log("net", net);
+      //drawNetwork(net);
+
+      var inputVals = [];
+      inputVals.length = this.inputHeaders.length;
+
+      var outputVals = [];
+      outputVals.length = this.outputHeaders.length;
+
+      // iterators
+      var k = 0,
+        j = 0;
+
+      var outputMap = {};
+      let val;
+      for (var i = 41; i < 60; i++) {
+        //parse inputs
+        //for (var i = 1; i < this.data.length; i++) {
+        for (j = 0; j < this.inputHeaders.length; j++) {
+          inputVals[j] = Number(this.data[i][this.inputHeaders[j].index]);
+        }
+        //parse outputs
+        for (k = 0; k < this.outputHeaders.length; k++) {
+          val = this.data[i][this.outputHeaders[k].index];
+          if (isNaN(val)) {
+            if (!outputMap[val])
+              outputMap[val] = Object.keys(outputMap).length + 1;
+            outputVals[k] = outputMap[val];
+          } else {
+            outputVals[k] = Number(this.data[i][this.outputVals[k].index]);
+          }
+        }
+        console.log("input vals", inputVals);
+        console.log("output vals", outputVals);
+        net.feedForward(inputVals);
+        net.backProp(outputVals);
+      }
     }
   }
 });
--- src/nodesGraph.js
+++ src/nodesGraph.js
@@ -1,24 +1,30 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+var canvas;
+var ctx;
 
-function Circle(x, y) {
-  this.x = x;
-  this.y = y;
-  this.draw = function() {
-    ctx.beginPath();
-    ctx.arc(x, y, 30, 0, 2 * Math.PI);
-    ctx.stroke();
-  };
+var circleSize = 30;
+var offsetX = circleSize * 2 + 40;
+var offsetY = circleSize * 2 + 10;
+
+function drawNodes(net) {
+  net.m_layers.forEach(function(layer, indexX) {
+    layer.forEach(function(node, indexY) {
+      ctx.beginPath();
+      ctx.arc(
+        offsetX + indexX * offsetX,
+        offsetY + indexY * offsetY,
+        circleSize,
+        0,
+        2 * Math.PI
+      );
+      ctx.stroke();
+    });
+  });
 }
 
-var list = [];
-for (var i = 0; i < 3; i++) {
-  for (var j = 0; j < 3; j++) {
-    var c = new Circle(40 + i * 85, 40 + j * 85);
-    list.push(c);
-  }
-}
+function drawNetwork(net) {
+  canvas = document.getElementById("myCanvas");
+  ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-for (var k = 0; k < list.length; k++) {
-  list[k].draw();
+  drawNodes(net);
 }
